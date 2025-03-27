f4ec4804cbb007008aeca4ef9aa638b562231400
jarek <iaroslav.pavlov@unibas.ch>
2025-03-27 13:25:06 +0100

    Adding new fields

--- models/mime_type.go
+++ models/mime_type.go
@@ -3,4 +3,5 @@ package models
 type MimeType struct {
 	Id        string
 	FileCount int64
+	FilesSize int64
 }
--- models/pronom.go
+++ models/pronom.go
@@ -3,4 +3,5 @@ package models
 type Pronom struct {
 	Id        string
 	FileCount int64
+	FilesSize int64
 }
